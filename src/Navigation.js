4ff938fac4423bf3b22544e27a21273096a4769e
Ville Välimäki <vjvalima@gmail.com>
2022-06-08 21:53:35 +0300

    improvement: add ci workflow and nav link base paths

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,22 +8,22 @@ function Navigation() {
     <div className="ContentContainer MarginedSmall NavItems">
         <div className="ContentQuarter MarginedSmall">
             <div className="NavItem">
-                <a className="NavLink" onClick={() => navigate('/', { replace: true })}>Enclosures</a>
+                <a href="/" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/', { replace: true })}}>Enclosures</a>
             </div>
         </div>
         <div className="ContentQuarter MarginedSmall">
             <div className="NavItem">
-                <a className="NavLink" onClick={() => navigate('/crossovers', { replace: true })}>Crossovers</a>
+                <a href="/crossovers" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/crossovers', { replace: true })}}>Crossovers</a>
             </div>
         </div>
         <div className="ContentQuarter MarginedSmall">
             <div className="NavItem">
-                <a className="NavLink"onClick={() => navigate('/amplifiers', { replace: true })}>Amplifiers</a>
+                <a href="/amplifiers" className="NavLink"onClick={(event) => {event.preventDefault(); navigate('/amplifiers', { replace: true })}}>Amplifiers</a>
             </div>
         </div>
         <div className="ContentQuarter MarginedSmall">
             <div className="NavItem">
-                <a className="NavLink"onClick={() => navigate('/links', { replace: true })}>Links</a>
+                <a href="/links" className="NavLink"onClick={(event) => {event.preventDefault(); navigate('/links', { replace: true })}}>Links</a>
             </div>
         </div>
     </div>
