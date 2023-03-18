d4c0eec23e8c5e73457faa47f45d1282eeb37a2c
Ville Välimäki <vjvalima@gmail.com>
2023-03-18 16:54:02 +0200

    improvement: classname updated

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -24,7 +24,7 @@ class Links extends React.Component<IProps, IState> {
                     <h2 className="PaddingMedium">Links and resources</h2>
                     <p>Since this site is nothing else but a small glimpse of what is related to speaker designing, how to address correct frequencies to right elements and give enough power for the elements; we have here a list of hopefully helpful links related to diy hifi.</p>
                 </div>
-                <div className="V2Content V2HPaddedOnSmall">
+                <div className="V2ContentFull V2HPaddedOnSmall">
                 {
                     this.state.links?.map((link: ISource, index: number) => {
                         return (
