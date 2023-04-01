9b4279108fb6628d2e46a3ad5761501ccc6c3838
Ville Välimäki <vjvalima@gmail.com>
2023-04-01 10:28:21 +0300

    improvement: updated header and navigation styles

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -2,8 +2,8 @@ import '../styles/Header.css';
 
 function Header() {
   return (
-    <div className="ContentContainer MarginedTop MarginedSmall">
-        <div className="ContentFull CenteredOnMobile">
+    <div className="ContentContainer">
+        <div className="V2ContentFull V2HPaddedOnSmall Header">
             <h1 className="Header1">Speaker design <span>Enclosures and Crossovers</span></h1>
         </div>
     </div>
--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -5,25 +5,27 @@ import { useNavigate } from "react-router-dom";
 function Navigation() {
     const navigate = useNavigate();
     return (
-    <div className="ContentContainer MarginedSmall NavItems">
-        <div className="ContentQuarter MarginedSmall">
-            <div className="NavItem">
-                <a href="/" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/', { replace: true })}}>Enclosures</a>
+    <div className="ContentContainer">
+        <div className="V2ContentFull V2VPaddingLarge">
+            <div className="V2ContentQuarter V2VPaddingMediumOnSmall">
+                <div className="NavItem">
+                    <a href="/" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/', { replace: true })}}>Enclosures</a>
+                </div>
             </div>
-        </div>
-        <div className="ContentQuarter MarginedSmall">
-            <div className="NavItem">
-                <a href="/speakers/src/pages/Crossovers.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/crossovers', { replace: true })}}>Crossovers</a>
+            <div className="V2ContentQuarter V2VPaddingMediumOnSmall">
+                <div className="NavItem">
+                    <a href="/speakers/src/pages/Crossovers.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/crossovers', { replace: true })}}>Crossovers</a>
+                </div>
             </div>
-        </div>
-        <div className="ContentQuarter MarginedSmall">
-            <div className="NavItem">
-                <a href="/speakers/src/pages/Amplifiers.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/amplifiers', { replace: true })}}>Amplifiers</a>
+            <div className="V2ContentQuarter">
+                <div className="NavItem">
+                    <a href="/speakers/src/pages/Amplifiers.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/amplifiers', { replace: true })}}>Amplifiers</a>
+                </div>
             </div>
-        </div>
-        <div className="ContentQuarter MarginedSmall">
-            <div className="NavItem">
-                <a href="/speakers/src/pages/Links.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/links', { replace: true })}}>Links</a>
+            <div className="V2ContentQuarter">
+                <div className="NavItem">
+                    <a href="/speakers/src/pages/Links.tsx" className="NavLink" onClick={(event) => {event.preventDefault(); navigate('/links', { replace: true })}}>Links</a>
+                </div>
             </div>
         </div>
     </div>
